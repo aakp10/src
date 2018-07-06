095dd908147d68015d92b411a2e2d98e55a78304
Karuna Grewal <karunagrewal98@gmail.com>
2018-07-07 02:05:25 +0530

    Fix coding style issues

--- sys/dev/i2c/tea5767.c
+++ sys/dev/i2c/tea5767.c
@@ -1,5 +1,7 @@
 #include <sys/cdefs.h>
 
+#include <sys/proc.h>
+#include <sys/kernel.h>
 #include <sys/param.h>
 #include <sys/device.h>
 #include <sys/errno.h>
@@ -12,10 +14,8 @@
 #include <dev/radio_if.h>
 
 #include "tea5767reg.h"
-#include <sys/proc.h>
-/**
- * Radio hw if functions' prototype
- */
+
+
 static int tea5767_get_info(void *, struct radio_info *);
 static int tea5767_set_info(void *, struct radio_info *);
 static int tea5767_search(void *, int);
@@ -31,7 +31,7 @@ static const struct radio_hw_if tea5767_hw_if = {
     tea5767_search
 };
 
-struct tea5767_tune_{
+struct tea5767_tune{
     int mute;
     uint32_t freq;
     int stereo;
@@ -41,21 +41,18 @@ struct tea5767_tune_{
     uint8_t read_pll[2];
     bool is_search_complete;
 };
-typedef struct tea5767_tune_ tea5767_tune;
-
-struct tea5767_softc_{
+struct tea5767_softc{
     device_t sc_dev;
     /* TUNABLE PROPERTIES */
-    tea5767_tune tune;
+    struct tea5767_tune tune;
     /* I2C bus controller */
     i2c_tag_t sc_i2c_tag;
     /* Device addr on I2C */
-    i2c_addr_t sc_addr;     //uint16_t => i2c_addr_t
+    i2c_addr_t sc_addr;
 };
-typedef struct tea5767_softc_ tea5767_softc;
 
-CFATTACH_DECL_NEW(tea5767radio, sizeof(tea5767_softc),
-    tea5767_match, tea5767_attach, NULL, NULL);
+CFATTACH_DECL_NEW(tea5767radio, sizeof(struct tea5767_softc), tea5767_match,
+    tea5767_attach, NULL, NULL);
 
 static int
 tea5767_match(device_t parent, cfdata_t cf, void *aux)
@@ -70,7 +67,7 @@ tea5767_match(device_t parent, cfdata_t cf, void *aux)
 static void
 tea5767_attach(device_t parent, device_t self, void *aux)
 {
-    tea5767_softc *sc = device_private(self);
+    struct tea5767_softc *sc = device_private(self);
     int tea5767_flags = device_cfdata(self)->cf_flags;
     struct i2c_attach_args *ia = aux;
 
@@ -96,75 +93,56 @@ tea5767_attach(device_t parent, device_t self, void *aux)
     radio_attach_mi(&tea5767_hw_if, sc, self);
 }
 
-static void
-tea5767_write(tea5767_softc *sc, uint8_t *reg)
+static int
+tea5767_write(struct tea5767_softc *sc, uint8_t *reg)
 {
-    if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL))
-    {
+    if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL)) {
         device_printf(sc->sc_dev, "Bus Acquiration failed \n");
-        return;
+        return 1;
     }
-    int exec_result = iic_exec(sc->sc_i2c_tag,
-                               I2C_OP_WRITE_WITH_STOP,
-                               sc->sc_addr,
-                               NULL,
-                               0,
-                               reg,
-                               5,
-                               I2C_F_POLL);
-    if (exec_result)
-    {
+    int exec_result = iic_exec(sc->sc_i2c_tag, I2C_OP_WRITE_WITH_STOP,
+                        sc->sc_addr, NULL, 0, reg, 5, I2C_F_POLL);
+    if (exec_result) {
         iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
         device_printf(sc->sc_dev, "write operation failed %d \n",exec_result);
+        return 1;
     }
     iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
+    return 0;
 }
 
 
 static void
-tea5767_set_properties(tea5767_softc *sc, uint8_t *reg)
+tea5767_set_properties(struct tea5767_softc *sc, uint8_t *reg)
 {
-    /**
-     *  set the PLL word
+    /*
+     * set the PLL word
      * TODO:
      * Extend this PLL word calc to  High side injection as well (Add fields to tea5767_tune)
      */
     memset(reg,0,5);
     uint16_t pll_word = 0;
 
-    if(!sc->tune.is_search_complete)
-    {
+    if(!sc->tune.is_search_complete) {
         if (!sc->tune.is_pll_set && !sc->tune.is_xtal_set)
             pll_word = 4 * (sc->tune.freq  - 225) * 1000 / 50000;
-        else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set)
-        {
+        else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set) {
             pll_word = 4 * (sc->tune.freq - 225) * 1000 / 32768;
             reg[3] = TEA5767_XTAL;
         }
-        else
-        {
+        else {
             pll_word = 4 * (sc->tune.freq - 225) * 1000 / 50000;
             reg[4] |= TEA5767_PLLREF;
         }
-
         reg[1] = pll_word & 0xff;
         reg[0] = (pll_word>>8) & 0x3f;
     }
-    else
-    {
+    else {
         reg[1] = sc->tune.read_pll[1];
         reg[0] |= sc->tune.read_pll[0];
     }
-    /**
-     * USE once the search function is active
-     * If "is_autosearch_complete" flag is set use the tuned PLL word
-     */
-    /*  reg[1] = sc->tune.read_pll[1];
-        reg[0] |= sc->tune.read_pll[0];
-    */
 
-    if (sc->tune.mute)
-    {
+    if (sc->tune.mute) {
         reg[0] |= TEA5767_MUTE;
         reg[2] |=TEA5767_MUTE_R | TEA5767_MUTE_L;
     }
@@ -174,17 +152,12 @@ tea5767_set_properties(tea5767_softc *sc, uint8_t *reg)
         reg[2] |= TEA5767_MONO;
     if(sc->tune.fm_band)
         reg[3] |= TEA5767_FM_BAND;
-    for(int i = 0; i < 5; i++)
-    {
-        device_printf(sc->sc_dev, "byte %d :%02x\n",i,reg[i]);
-    }
-
 }
 
 static int
 tea5767_get_info(void *v, struct radio_info *ri)
 {
-    tea5767_softc *sc = v;
+    struct tea5767_softc *sc = v;
     ri->mute = sc->tune.mute;
     ri->stereo = sc->tune.stereo;
     ri->freq = sc->tune.freq;
@@ -198,7 +171,7 @@ tea5767_get_info(void *v, struct radio_info *ri)
 static int
 tea5767_set_info(void *v, struct radio_info *ri)
 {
-    tea5767_softc *sc = v;
+    struct tea5767_softc *sc = v;
     sc->tune.mute = ri->mute;
     sc->tune.stereo = ri->stereo;
     if(sc->tune.freq != ri->freq)
@@ -206,42 +179,38 @@ tea5767_set_info(void *v, struct radio_info *ri)
     sc->tune.freq = ri->freq;
     tea5767_search(sc, 1);
     uint8_t reg[5];
-    tea5767_set_properties(sc,reg);
-    tea5767_write(sc,reg);
-    return 1;
+    tea5767_set_properties(sc, reg);
+    return tea5767_write(sc, reg);
 }
 
-static void
-tea5767_read(tea5767_softc *sc, uint8_t *reg)
+static int
+tea5767_read(struct tea5767_softc *sc, uint8_t *reg)
 {
-    if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL))
-    {
+    if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL)) {
         device_printf(sc->sc_dev, "Bus Acquiration failed \n");
-        return;
+        return 1;
     }
-    int exec_result = iic_exec(sc->sc_i2c_tag,
-                               I2C_OP_READ_WITH_STOP,
-                               sc->sc_addr,
-                               NULL,
-                               0,
-                               reg,
-                               5,
-                               I2C_F_POLL);
-    if (exec_result)
-    {
+
+    int exec_result = iic_exec(sc->sc_i2c_tag, I2C_OP_READ_WITH_STOP, sc->sc_addr,
+                        NULL, 0, reg, 5, I2C_F_POLL);
+
+    if (exec_result) {
         iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
         device_printf(sc->sc_dev, "read operation failed");
+        return 1;
     }
+
     iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
+    return 0;
 }
 
 static int
 tea5767_search(void *v, int dir)
 {
-    tea5767_softc *sc = v;
+    struct tea5767_softc *sc = v;
     uint8_t reg[5];
     tea5767_set_properties(sc, reg);
-    /**
+    /*
      * search activated
      * if dir 1 => search up
      * else : search down
@@ -253,14 +222,12 @@ tea5767_search(void *v, int dir)
     reg[2] |= TEA5767_SSL_1; /* Stop level for search*/
     tea5767_write(sc, reg);
 
-    /* ?? Try to use reg instead of read_reg - PLL bits are same in r/w mode ?? */
     uint8_t read_reg[5];
     memset(read_reg,0,5);
     tea5767_read(sc, read_reg);
 
-    while(!(read_reg[0]&TEA5767_READY_FLAG))
-    {
-        kpause("tea-station-search", true, 1, NULL);
+    while (!(read_reg[0] & TEA5767_READY_FLAG)) {
+        kpause("teasrch", true, 1 * hz, NULL);
         memset(read_reg,0,5);
         tea5767_read(sc,read_reg);
     }
