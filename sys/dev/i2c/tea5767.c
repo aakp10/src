5e90b57d3bfccd4e7cac556d4627e844c52a8185
Karuna Grewal <karunagrewal98@gmail.com>
2018-07-19 03:02:20 +0530

    Search + sensitivity changes

--- sys/dev/i2c/tea5767.c
+++ sys/dev/i2c/tea5767.c
@@ -38,8 +38,8 @@ struct tea5767_tune{
     int is_pll_set;
     int is_xtal_set;
     int fm_band; /* set = JAPAN */
-    uint8_t read_pll[2];
-    bool is_search_complete;
+    int lock;
+    int adc_stop_level;
 };
 struct tea5767_softc{
     device_t sc_dev;
@@ -81,7 +81,6 @@ tea5767_attach(device_t parent, device_t self, void *aux)
     sc->tune.is_xtal_set = 0;
     sc->sc_i2c_tag = ia->ia_tag;
     sc->sc_addr = ia->ia_addr;
-    memset(sc->tune.read_pll,0,2);
 
     if (tea5767_flags & TEA5767_PLL_FLAG)
         sc->tune.is_pll_set = 1;
@@ -112,76 +111,6 @@ tea5767_write(struct tea5767_softc *sc, uint8_t *reg)
 }
 
 
-static void
-tea5767_set_properties(struct tea5767_softc *sc, uint8_t *reg)
-{
-    /*
-     * set the PLL word
-     * TODO:
-     * Extend this PLL word calc to  High side injection as well (Add fields to tea5767_tune)
-     */
-    memset(reg,0,5);
-    uint16_t pll_word = 0;
-
-    if(!sc->tune.is_search_complete) {
-        if (!sc->tune.is_pll_set && !sc->tune.is_xtal_set)
-            pll_word = 4 * (sc->tune.freq  - 225) * 1000 / 50000;
-        else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set) {
-            pll_word = 4 * (sc->tune.freq - 225) * 1000 / 32768;
-            reg[3] = TEA5767_XTAL;
-        }
-        else {
-            pll_word = 4 * (sc->tune.freq - 225) * 1000 / 50000;
-            reg[4] |= TEA5767_PLLREF;
-        }
-        reg[1] = pll_word & 0xff;
-        reg[0] = (pll_word>>8) & 0x3f;
-    }
-    else {
-        reg[1] = sc->tune.read_pll[1];
-        reg[0] |= sc->tune.read_pll[0];
-    }
-
-    if (sc->tune.mute) {
-        reg[0] |= TEA5767_MUTE;
-        reg[2] |=TEA5767_MUTE_R | TEA5767_MUTE_L;
-    }
-
-    reg[3] |= TEA5767_SNC;
-    if (sc->tune.stereo == 0)
-        reg[2] |= TEA5767_MONO;
-    if(sc->tune.fm_band)
-        reg[3] |= TEA5767_FM_BAND;
-}
-
-static int
-tea5767_get_info(void *v, struct radio_info *ri)
-{
-    struct tea5767_softc *sc = v;
-    ri->mute = sc->tune.mute;
-    ri->stereo = sc->tune.stereo;
-    ri->freq = sc->tune.freq;
-
-    ri->caps = RADIO_CAPS_DETECT_STEREO|
-               RADIO_CAPS_SET_MONO|
-               RADIO_CAPS_HW_SEARCH;
-    return 0;
-}
-
-static int
-tea5767_set_info(void *v, struct radio_info *ri)
-{
-    struct tea5767_softc *sc = v;
-    sc->tune.mute = ri->mute;
-    sc->tune.stereo = ri->stereo;
-    if(sc->tune.freq != ri->freq)
-        sc->tune.is_search_complete = false;
-    sc->tune.freq = ri->freq;
-    uint8_t reg[5];
-    tea5767_set_properties(sc, reg);
-    return tea5767_write(sc, reg);
-}
-/*
 static int
 tea5767_read(struct tea5767_softc *sc, uint8_t *reg)
 {
@@ -202,12 +131,101 @@ tea5767_read(struct tea5767_softc *sc, uint8_t *reg)
     iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
     return 0;
 }
-*/
+
+static void
+tea5767_set_properties(struct tea5767_softc *sc, uint8_t *reg)
+{
+    /*
+     * set the PLL word
+     * TODO:
+     * Extend this PLL word calc to  High side injection as well (Add fields to tea5767_tune)
+     */
+    memset(reg,0,5);
+    uint16_t pll_word = 0;
+
+    if (!sc->tune.is_pll_set && !sc->tune.is_xtal_set)
+        pll_word = 4 * (sc->tune.freq  - 225) * 1000 / 50000;
+    else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set) {
+        pll_word = 4 * (sc->tune.freq - 225) * 1000 / 32768;
+        reg[3] = TEA5767_XTAL;
+    }
+    else {
+        pll_word = 4 * (sc->tune.freq - 225) * 1000 / 50000;
+        reg[4] |= TEA5767_PLLREF;
+    }
+    reg[1] = pll_word & 0xff;
+    reg[0] = (pll_word>>8) & 0x3f;
+
+    if (sc->tune.mute) {
+        reg[0] |= TEA5767_MUTE;
+        reg[2] |=TEA5767_MUTE_R | TEA5767_MUTE_L;
+    }
+
+    reg[3] |= TEA5767_SNC;
+    if (sc->tune.stereo == 0)
+        reg[2] |= TEA5767_MONO;
+    if(sc->tune.fm_band)
+        reg[3] |= TEA5767_FM_BAND;
+}
+
+static int
+tea5767_get_info(void *v, struct radio_info *ri)
+{
+    struct tea5767_softc *sc = v;
+    uint8_t reg[5];
+    tea5767_read(sc, reg);
+
+    int pll_word = reg[1] | (reg[0] & 0x3f) << 8;
+    if (!sc->tune.is_pll_set && !sc->tune.is_xtal_set)
+        sc->tune.freq = pll_word * 50 / 4 + 225;
+    else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set)
+        sc->tune.freq = pll_word * 32768 / 4000 + 225;
+    else
+        sc->tune.freq = pll_word * 50 / 4 + 225;
+
+    ri->mute = sc->tune.mute;
+    ri->stereo = sc->tune.stereo;
+    ri->freq = sc->tune.freq;
+    ri->lock = sc->tune.lock;
+
+    ri->caps = RADIO_CAPS_DETECT_STEREO|
+               RADIO_CAPS_SET_MONO|
+               RADIO_CAPS_HW_SEARCH|
+               RADIO_CAPS_LOCK_SENSITIVITY;
+    return 0;
+}
+
+static int
+tea5767_set_info(void *v, struct radio_info *ri)
+{
+    struct tea5767_softc *sc = v;
+    int adc_conversion;
+    sc->tune.mute = ri->mute;
+    sc->tune.stereo = ri->stereo;
+    sc->tune.freq = ri->freq;
+    sc->tune.lock = ri->lock;
+    adc_conversion = (ri->lock-60) * 3 / 30;
+    switch(adc_conversion) {
+        case 0: sc->tune.adc_stop_level = TEA5767_SSL_3;
+                break;
+        case 1: sc->tune.adc_stop_level = TEA5767_SSL_2;
+                break;
+        case 2: sc->tune.adc_stop_level = TEA5767_SSL_1;
+                break;
+    }
+    uint8_t reg[5];
+    tea5767_set_properties(sc, reg);
+    return tea5767_write(sc, reg);
+}
+
 static int
 tea5767_search(void *v, int dir)
 {
     struct tea5767_softc *sc = v;
     uint8_t reg[5];
+
+    /* increment frequency to search for the next frequency*/
+    sc->tune.freq += 100;
     tea5767_set_properties(sc, reg);
     /*
      * search activated
@@ -219,7 +237,12 @@ tea5767_search(void *v, int dir)
 
     if (dir)
         reg[2] |= TEA5767_SUD;
-    reg[2] |= TEA5767_SSL_3; /* Stop level for search*/
+    reg[2] |= sc->tune.adc_stop_level; /* Stop level for search*/
     tea5767_write(sc, reg);
+
+    memset(reg, 0, 5);
+    while (tea5767_read(sc, reg), !(reg[0] & TEA5767_READY_FLAG)) {
+        kpause("teasrch", true, hz/100, NULL);
+    }
     return 0;
 }
