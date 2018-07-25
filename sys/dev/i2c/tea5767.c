3db0a6c02c76ea84a7d29a9916bc011c398b6181
Karuna Grewal <karunagrewal98@gmail.com>
2018-07-26 02:35:59 +0530

    Check IF before proceeding with autosearch

--- sys/dev/i2c/tea5767.c
+++ sys/dev/i2c/tea5767.c
@@ -1,3 +1,32 @@
+/*-
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Karuna Grewal.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <sys/cdefs.h>
 
 #include <sys/proc.h>
@@ -24,219 +53,257 @@ static int tea5767_match(device_t, cfdata_t, void *);
 static void tea5767_attach(device_t, device_t, void *);
 
 static const struct radio_hw_if tea5767_hw_if = {
-    NULL,
-    NULL,
-    tea5767_get_info,
-    tea5767_set_info,
-    tea5767_search
+	NULL,
+	NULL,
+	tea5767_get_info,
+	tea5767_set_info,
+	tea5767_search
 };
 
 struct tea5767_tune{
-    int mute;
-    uint32_t freq;
-    int stereo;
-    int is_pll_set;
-    int is_xtal_set;
-    int fm_band; /* set = JAPAN */
-    int lock;
-    int adc_stop_level;
+	int mute;
+	uint32_t freq;
+	int stereo;
+	bool is_pll_set;
+	bool is_xtal_set;
+	bool is_force_srch;
+	int fm_band; /* set = JAPAN */
+	int lock;
+	int adc_stop_level;
 };
 struct tea5767_softc{
-    device_t sc_dev;
-    /* TUNABLE PROPERTIES */
-    struct tea5767_tune tune;
-    /* I2C bus controller */
-    i2c_tag_t sc_i2c_tag;
-    /* Device addr on I2C */
-    i2c_addr_t sc_addr;
+	device_t sc_dev;
+	/* TUNABLE PROPERTIES */
+	struct tea5767_tune tune;
+	/* I2C bus controller */
+	i2c_tag_t sc_i2c_tag;
+	/* Device addr on I2C */
+	i2c_addr_t sc_addr;
 };
 
 CFATTACH_DECL_NEW(tea5767radio, sizeof(struct tea5767_softc), tea5767_match,
-    tea5767_attach, NULL, NULL);
+	tea5767_attach, NULL, NULL);
 
 static int
 tea5767_match(device_t parent, cfdata_t cf, void *aux)
 {
-    struct i2c_attach_args *ia = aux;
+	struct i2c_attach_args *ia = aux;
 
-    if (ia->ia_addr == TEA5767_ADDR)
-        return 1;
-    return 0;
+	if (ia->ia_addr == TEA5767_ADDR)
+		return 1;
+	return 0;
 }
 
 static void
 tea5767_attach(device_t parent, device_t self, void *aux)
 {
-    struct tea5767_softc *sc = device_private(self);
-    int tea5767_flags = device_cfdata(self)->cf_flags;
-    struct i2c_attach_args *ia = aux;
+	struct tea5767_softc *sc = device_private(self);
+	int tea5767_flags = device_cfdata(self)->cf_flags;
+	struct i2c_attach_args *ia = aux;
 
-    aprint_normal(": TEA5767 Radio\n");
+	aprint_normal(": TEA5767 Radio\n");
 
-    sc->sc_dev = self;
-    sc->tune.mute = 0;
-    sc->tune.freq = MIN_FM_FREQ;
-    sc->tune.stereo = 1;
-    sc->tune.is_pll_set = 0;
-    sc->tune.is_xtal_set = 0;
-    sc->sc_i2c_tag = ia->ia_tag;
-    sc->sc_addr = ia->ia_addr;
+	sc->sc_dev = self;
+	sc->tune.mute = 0;
+	sc->tune.freq = MIN_FM_FREQ;
+	sc->tune.stereo = 1;
+	sc->tune.is_pll_set = false;
+	sc->tune.is_xtal_set = false;
+	sc->tune.is_force_srch = false;
+	sc->sc_i2c_tag = ia->ia_tag;
+	sc->sc_addr = ia->ia_addr;
 
-    if (tea5767_flags & TEA5767_PLL_FLAG)
-        sc->tune.is_pll_set = 1;
-    if (tea5767_flags & TEA5767_XTAL_FLAG)
-        sc->tune.is_xtal_set = 1;
-    if (tea5767_flags & TEA5767_JAPAN_FM_FLAG)
-        sc->tune.fm_band = 1;
+	if (tea5767_flags & TEA5767_PLL_FLAG)
+		sc->tune.is_pll_set = true;
+	if (tea5767_flags & TEA5767_XTAL_FLAG)
+		sc->tune.is_xtal_set = true;
+	if (tea5767_flags & TEA5767_JAPAN_FM_FLAG)
+		sc->tune.fm_band = 1;
+	if (tea5767_flags & TEA5767_FORCE_SRCH_FLAG)
+		sc->tune.is_force_srch = true;
 
-    radio_attach_mi(&tea5767_hw_if, sc, self);
+	radio_attach_mi(&tea5767_hw_if, sc, self);
 }
 
 static int
 tea5767_write(struct tea5767_softc *sc, uint8_t *reg)
 {
-    if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL)) {
-        device_printf(sc->sc_dev, "Bus Acquiration failed \n");
-        return 1;
-    }
-    int exec_result = iic_exec(sc->sc_i2c_tag, I2C_OP_WRITE_WITH_STOP,
-                        sc->sc_addr, NULL, 0, reg, 5, I2C_F_POLL);
-    if (exec_result) {
-        iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
-        device_printf(sc->sc_dev, "write operation failed %d \n",exec_result);
-        return 1;
-    }
-    iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
-    return 0;
+	if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL)) {
+		device_printf(sc->sc_dev, "Bus Acquiration failed \n");
+		return 1;
+	}
+	int exec_result = iic_exec(sc->sc_i2c_tag, I2C_OP_WRITE_WITH_STOP,
+						sc->sc_addr, NULL, 0, reg, 5, I2C_F_POLL);
+	if (exec_result) {
+		iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
+		device_printf(sc->sc_dev, "write operation failed %d \n",exec_result);
+		return 1;
+	}
+	iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
+	return 0;
 }
 
 
 static int
 tea5767_read(struct tea5767_softc *sc, uint8_t *reg)
 {
-    if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL)) {
-        device_printf(sc->sc_dev, "Bus Acquiration failed \n");
-        return 1;
-    }
+	if (iic_acquire_bus(sc->sc_i2c_tag, I2C_F_POLL)) {
+		device_printf(sc->sc_dev, "Bus Acquiration failed \n");
+		return 1;
+	}
 
-    int exec_result = iic_exec(sc->sc_i2c_tag, I2C_OP_READ_WITH_STOP, sc->sc_addr,
-                        NULL, 0, reg, 5, I2C_F_POLL);
+	int exec_result = iic_exec(sc->sc_i2c_tag, I2C_OP_READ_WITH_STOP, sc->sc_addr,
+						NULL, 0, reg, 5, I2C_F_POLL);
 
-    if (exec_result) {
-        iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
-        device_printf(sc->sc_dev, "read operation failed");
-        return 1;
-    }
+	if (exec_result) {
+		iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
+		device_printf(sc->sc_dev, "read operation failed");
+		return 1;
+	}
 
-    iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
-    return 0;
+	iic_release_bus(sc->sc_i2c_tag, I2C_F_POLL);
+	return 0;
+}
+
+static void
+tea5767_freq_to_pll(struct tea5767_tune tune, uint8_t *buf)
+{
+	uint16_t pll_word = 0;
+	if (!tune.is_pll_set && !tune.is_xtal_set)
+		pll_word = 4 * (tune.freq  - 225) * 1000 / 50000;
+	else if (!tune.is_pll_set && tune.is_xtal_set) {
+		pll_word = 4 * (tune.freq - 225) * 1000 / 32768;
+		buf[3] = TEA5767_XTAL;
+	}
+	else {
+		pll_word = 4 * (tune.freq - 225) * 1000 / 50000;
+		buf[4] |= TEA5767_PLLREF;
+	}
+	buf[1] = pll_word & 0xff;
+	buf[0] = (pll_word>>8) & 0x3f;
 }
 
 static void
 tea5767_set_properties(struct tea5767_softc *sc, uint8_t *reg)
 {
-    /*
-     * set the PLL word
-     * TODO:
-     * Extend this PLL word calc to  High side injection as well (Add fields to tea5767_tune)
-     */
-    memset(reg,0,5);
-    uint16_t pll_word = 0;
+	/*
+	 * set the PLL word
+	 * TODO:
+	 * Extend this PLL word calc to  High side injection as well (Add fields to tea5767_tune)
+	 */
+	memset(reg,0,5);
+	tea5767_freq_to_pll(sc->tune, reg);
+	if (sc->tune.mute) {
+		reg[0] |= TEA5767_MUTE;
+		reg[2] |=TEA5767_MUTE_R | TEA5767_MUTE_L;
+	}
 
-    if (!sc->tune.is_pll_set && !sc->tune.is_xtal_set)
-        pll_word = 4 * (sc->tune.freq  - 225) * 1000 / 50000;
-    else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set) {
-        pll_word = 4 * (sc->tune.freq - 225) * 1000 / 32768;
-        reg[3] = TEA5767_XTAL;
-    }
-    else {
-        pll_word = 4 * (sc->tune.freq - 225) * 1000 / 50000;
-        reg[4] |= TEA5767_PLLREF;
-    }
-    reg[1] = pll_word & 0xff;
-    reg[0] = (pll_word>>8) & 0x3f;
+	reg[3] |= TEA5767_SNC;
+	if (sc->tune.stereo == 0)
+		reg[2] |= TEA5767_MONO;
+	if(sc->tune.fm_band)
+		reg[3] |= TEA5767_FM_BAND;
+}
 
-    if (sc->tune.mute) {
-        reg[0] |= TEA5767_MUTE;
-        reg[2] |=TEA5767_MUTE_R | TEA5767_MUTE_L;
-    }
 
-    reg[3] |= TEA5767_SNC;
-    if (sc->tune.stereo == 0)
-        reg[2] |= TEA5767_MONO;
-    if(sc->tune.fm_band)
-        reg[3] |= TEA5767_FM_BAND;
+static bool
+tea5767_if_check(void *v)
+{
+	struct tea5767_softc *sc = v;
+	uint8_t reg[5];
+	tea5767_set_properties(sc, reg);
+	tea5767_write(sc, reg);
+	memset(reg,0,5);
+	delay(1000);
+	tea5767_read(sc,reg);
+	if ((reg[2] & 0x7f) == 0x37)
+		return true;
+	return false;    
+}
+
+static void
+tea5767_pll_to_freq(struct tea5767_tune *tune, uint8_t *buf)
+{
+	int pll_word = buf[1] | (buf[0] & 0x3f) << 8;
+	if (!tune->is_pll_set && !tune->is_xtal_set)
+		tune->freq = pll_word * 50 / 4 + 225;
+	else if (!tune->is_pll_set && tune->is_xtal_set)
+		tune->freq = pll_word * 32768 / 4000 + 225;
+	else
+		tune->freq = pll_word * 50 / 4 + 225;
 }
 
 static int
 tea5767_get_info(void *v, struct radio_info *ri)
 {
-    struct tea5767_softc *sc = v;
-    uint8_t reg[5];
-    tea5767_read(sc, reg);
+	struct tea5767_softc *sc = v;
+	uint8_t reg[5];
+	tea5767_read(sc, reg);
 
-    int pll_word = reg[1] | (reg[0] & 0x3f) << 8;
-    if (!sc->tune.is_pll_set && !sc->tune.is_xtal_set)
-        sc->tune.freq = pll_word * 50 / 4 + 225;
-    else if (!sc->tune.is_pll_set && sc->tune.is_xtal_set)
-        sc->tune.freq = pll_word * 32768 / 4000 + 225;
-    else
-        sc->tune.freq = pll_word * 50 / 4 + 225;
+	tea5767_pll_to_freq(&sc->tune, reg);
+	ri->mute = sc->tune.mute;
+	ri->stereo = sc->tune.stereo;
+	ri->freq = sc->tune.freq;
+	ri->lock = sc->tune.lock;
 
-    ri->mute = sc->tune.mute;
-    ri->stereo = sc->tune.stereo;
-    ri->freq = sc->tune.freq;
-    ri->lock = sc->tune.lock;
-
-    ri->caps = RADIO_CAPS_DETECT_STEREO|
-               RADIO_CAPS_SET_MONO|
-               RADIO_CAPS_HW_SEARCH|
-               RADIO_CAPS_LOCK_SENSITIVITY;
-    return 0;
+	ri->caps = RADIO_CAPS_DETECT_STEREO|
+			   RADIO_CAPS_SET_MONO|
+			   RADIO_CAPS_HW_SEARCH|
+			   RADIO_CAPS_LOCK_SENSITIVITY;
+	if(!tea5767_if_check(v) && !sc->tune.is_force_srch)
+		ri->caps &= ~RADIO_CAPS_HW_SEARCH;
+	return 0;
 }
 
 static int
 tea5767_set_info(void *v, struct radio_info *ri)
 {
-    struct tea5767_softc *sc = v;
-    int adc_conversion;
-    sc->tune.mute = ri->mute;
-    sc->tune.stereo = ri->stereo;
-    sc->tune.freq = ri->freq;
-    sc->tune.lock = ri->lock;
-    adc_conversion = (ri->lock-60) * 3 / 30;
-    switch(adc_conversion) {
-        case 0: sc->tune.adc_stop_level = TEA5767_SSL_3;
-                break;
-        case 1: sc->tune.adc_stop_level = TEA5767_SSL_2;
-                break;
-        case 2: sc->tune.adc_stop_level = TEA5767_SSL_1;
-                break;
-    }
-    uint8_t reg[5];
-    tea5767_set_properties(sc, reg);
-    tea5767_write(sc, reg);
-    /*
-    memset(reg,0,5);
-    tea5767_read(sc,reg);
-    device_printf(sc->sc_dev, "adc level %d \n",(reg[3] & 0xf0)>>4);
-    */
-    return 0;
+	struct tea5767_softc *sc = v;
+	int adc_conversion;
+	sc->tune.mute = ri->mute;
+	sc->tune.stereo = ri->stereo;
+	sc->tune.freq = ri->freq;
+	sc->tune.lock = ri->lock;
+	adc_conversion = (ri->lock-60) * 3 / 30;
+	switch(adc_conversion) {
+		case 0: sc->tune.adc_stop_level = TEA5767_SSL_3;
+				break;
+		case 1: sc->tune.adc_stop_level = TEA5767_SSL_2;
+				break;
+		case 2: sc->tune.adc_stop_level = TEA5767_SSL_1;
+				break;
+	}
+	uint8_t reg[5];
+	tea5767_set_properties(sc, reg);
+	tea5767_write(sc, reg);
+	return 0;
 }
 
 static int
 tea5767_search(void *v, int dir)
 {
-    struct tea5767_softc *sc = v;
-    uint8_t reg[5];
-    memset(reg,0,5);
-    while(tea5767_read(sc,reg) ,(reg[3] & 0xf0) != /* ??misc ADC??*/)
-    {
-        sc->tune.freq = sc->tune.freq >= 107500 ? 87500 : sc->tune.freq + 100;
-        tea5767_set_properties(sc, reg);
-        tea5767_write(sc, reg);
-        memset(reg,0,5);
-    }
-    return 0;
-}
+	struct tea5767_softc *sc = v;
+	uint8_t reg[5];
 
+	/* increment frequency to search for the next frequency*/
+	sc->tune.freq = sc->tune.freq >= 107500 ? 87500 : sc->tune.freq + 100;
+	tea5767_set_properties(sc, reg);
+	/*
+	 * search activated
+	 * if dir 1 => search up
+	 * else : search down
+	 */
+
+	reg[0] |= TEA5767_SEARCH;
+
+	if (dir)
+		reg[2] |= TEA5767_SUD;
+	reg[2] |= sc->tune.adc_stop_level; /* Stop level for search*/
+	tea5767_write(sc, reg);
+
+	memset(reg, 0, 5);
+	while (tea5767_read(sc, reg), !(reg[0] & TEA5767_READY_FLAG)) {
+		kpause("teasrch", true, hz/100, NULL);
+	}
+	return 0;
+}
